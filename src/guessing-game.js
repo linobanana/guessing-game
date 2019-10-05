581aa1304cf54f0375db0dc01511bc22ea75a11a
Anhelina Lakhtanava <linobanana@gmail.com>
2019-10-05 23:23:20 +0300

    task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.resultOfSearch = undefined;
+        this.min = undefined;
+        this.max = undefined;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.resultOfSearch = Math.round((this.min + this.max) / 2);
+        return this.resultOfSearch;
     }
 
     lower() {
-
+        this.max = this.resultOfSearch;
     }
 
     greater() {
-
+        this.min = this.resultOfSearch;
     }
 }
 
